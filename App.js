af7fa812ff94ff51eb3e2099e6cfddef73d1e921
Nifadia Lillahkul <6310110704@psu.ac.th>
2022-08-28 17:49:31 +0700

    init

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const doIt = () => {
+    console.log("Hello from console")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
