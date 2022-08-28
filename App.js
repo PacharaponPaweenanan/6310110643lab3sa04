b41a7b7936d10c10c10725d58e4cdb35160ee9c9
PacharaponPaweenanan <6310110643@psu.ac.th>
2022-08-28 18:12:08 +0700

    init

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>Hello World I am peem</Text>
       <StatusBar style="auto" />
     </View>
   );
